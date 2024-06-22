6132e00207b59a1d30d99a0217bbe8e1a2553263
msantosg <mariosantos1001@hotmail.com>
2024-06-22 17:15:38 -0600

    Agregar método de insertar y actualizar configuración de reportería

--- Prototipo Proyecto Web/App_Code/clsConsultas.cs
+++ Prototipo Proyecto Web/App_Code/clsConsultas.cs
@@ -31,11 +31,14 @@ namespace Prototipo_Proyecto_Web.App_Code
                     adapter = new SqlDataAdapter(cmd);
                     adapter.Fill(dt);
                 }
+
+                clsLog.EscribeLogInOut("Finalizo consulta de datos del monitor exitosamente", clsUtils.GetCurrentMethodName(), clsLog.tInOut.OUT, guid);
             }
             catch (Exception ex)
             {
                 dt = null;  
                 clsLog.EscribeLogErr(ex, clsUtils.GetCurrentMethodName());
+                clsLog.EscribeLogInOut("Finalizo consulta de datos del monitor con error", clsUtils.GetCurrentMethodName(), clsLog.tInOut.OUT, guid);
             }
             finally
             {
@@ -62,11 +65,14 @@ namespace Prototipo_Proyecto_Web.App_Code
                     adapter = new SqlDataAdapter(cmd);
                     adapter.Fill(dt);
                 }
+
+                clsLog.EscribeLogInOut("Finalizo consulta de datos del configurador exitosamente", clsUtils.GetCurrentMethodName(), clsLog.tInOut.OUT, guid);
             }
             catch (Exception ex)
             {
                 dt = null;
                 clsLog.EscribeLogErr(ex, clsUtils.GetCurrentMethodName());
+                clsLog.EscribeLogInOut("Finalizo consulta de datos del configurador con error", clsUtils.GetCurrentMethodName(), clsLog.tInOut.OUT, guid);
             }
             finally
             {
@@ -74,6 +80,52 @@ namespace Prototipo_Proyecto_Web.App_Code
             }
 
             return dt;
+        }
+
+        public int InsUpConfRPT(string jsonRegistros)
+        {
+            int res = 0;
+            clsCon = new clsConexion();
+            var guid = clsLog.EscribeLogInOut("Insertando o actualizando configuración de reportes", clsUtils.GetCurrentMethodName(), clsLog.tInOut.IN);
+            try
+            {
+                if (clsCon.Conectar())
+                {
+                    cmd = new SqlCommand();
+                    cmd.Connection = clsCon.GetConnection();
+                    cmd.CommandType = CommandType.StoredProcedure;
+                    cmd.CommandText = "sp_insoact_configuracion";
+                    cmd.Parameters.Add(new SqlParameter()
+                    {
+                        ParameterName = "@JSONREGISTROS",
+                        SqlDbType = SqlDbType.NVarChar,
+                        Value = jsonRegistros,
+                        Size = 32000,
+                        Direction = ParameterDirection.Input
+                    });
+
+                    cmd.Parameters.Add(new SqlParameter()
+                    {
+                        ParameterName = "@ID_CNF",
+                        SqlDbType = SqlDbType.Int,
+                        Direction = ParameterDirection.Output,
+                        Size = 8
+                    });
+
+                    cmd.ExecuteNonQuery();
+
+                    res = Convert.ToInt32(cmd.Parameters["@ID_CNF"].Value.ToString());
+                }
+
+                clsLog.EscribeLogInOut("Finalicé correctamente la insercíón o actualización la información en la configuración de reportes", clsUtils.GetCurrentMethodName(), clsLog.tInOut.OUT, guid);
+            }
+            catch(Exception ex)
+            {
+                res = 0;
+                clsLog.EscribeLogErr(ex, clsUtils.GetCurrentMethodName());
+                clsLog.EscribeLogInOut("No se inserto o actualizo la información en la configuración de reportes", clsUtils.GetCurrentMethodName(), clsLog.tInOut.OUT, guid);
+            }
+            return res;
         }
     }
 }
